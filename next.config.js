ce9574d08cf568b1132878e8d9684f25c4065b6d
SONG YUJIN <j_jongleur@naver.com>
2023-01-11 21:44:15 +0900

    SW-74 feat(diary):Add Spinner
    로딩시 표시되는 Spinner 추가 - svg webpack

--- next.config.js
+++ next.config.js
@@ -15,4 +15,13 @@ module.exports = {
   images: {
     domains: ['source.unsplash.com']
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    })
+
+    return config
+  },
 }
