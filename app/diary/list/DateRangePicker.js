cf2d52082936f1f7be47b478e8257216a5eae538
KIM-JOOHYUN <kjh99723@gmail.com>
2023-01-13 12:18:47 +0900

    SW-81 feat(diary_search):Set Grid List Search Data

--- app/diary/list/DateRangePicker.js
+++ app/diary/list/DateRangePicker.js
@@ -4,7 +4,7 @@ import { useState } from 'react';
 import dynamic from 'next/dynamic';
 import "react-datepicker/dist/react-datepicker.css";
 
-const DateRangePicker = () => {
+const DateRangePicker = (props) => {
     
     const DatePicker = dynamic(() => import('react-datepicker'), { ssr: false, })
     const [startDate, setStartDate] = useState(new Date());
@@ -13,13 +13,13 @@ const DateRangePicker = () => {
     return (
         <div className='grid grid-cols-7 gap-2'>
             <div className='col-span-3'>
-                <DatePicker popperProps={{ strategy: "fixed" }} className='w-full h-10 p-2 text-xs sm:text-base rounded-2xl' selected={startDate} onChange={(date) => setStartDate(date)} />
+                <DatePicker popperProps={{ strategy: "fixed" }} className='w-full h-10 p-2 text-xs sm:text-base rounded-2xl' selected={startDate} onChange={(date) => {props.setStartDate(props.setDateFormat(date)); setStartDate(date);}} />
             </div>
             <div className='flex items-center justify-center col-span-1'>
                 ~
             </div>
             <div className='col-span-3'>
-                <DatePicker popperProps={{ strategy: "fixed" }} className='w-full h-10 p-2 text-xs sm:text-base rounded-2xl' selected={endDate} onChange={(date) => setEndDate(date)} />
+                <DatePicker popperProps={{ strategy: "fixed" }} className='w-full h-10 p-2 text-xs sm:text-base rounded-2xl' selected={endDate} onChange={(date) => {props.setEndDate(props.setDateFormat(date)); setEndDate(date);}} />
             </div>
         </div>
     );
--- app/diary/list/grid/SearchBox.js
+++ app/diary/list/grid/SearchBox.js
@@ -1,7 +1,48 @@
-import DateRangePicker from "./DateRangePicker";
+'use client';
+
+import DateRangePicker from "../DateRangePicker";
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline'
+import {useState} from "react";
 
 const SearchBox = () => {
+    // 날짜 형식을 YYYY-MM-DD로 설정
+    function setDateFormat(date){
+        let result = date.getFullYear().toString() + "-" + (date.getMonth()+1).toString().padStart(2,'0') + "-" + date.getDate().toString().padStart(2,'0')
+        return result
+    }
+
+    // 검색 옵션
+    const [query,setQuery] = useState("");
+    const [emotion, setEmotion] = useState("");
+    const [sort, setSort] = useState("");
+    const [startDate, setStartDate] = useState(setDateFormat(new Date(1999,0,1)));
+    const [endDate, setEndDate] = useState(setDateFormat(new Date()));
+
+    // 검색 옵션 설정값 가져오기
+    // 검색어
+    const onQueryChange =(e)=>{
+        setQuery(e.target.value);
+    }
+    // 감정
+    const onEmotionChange =(e)=>{
+        setEmotion(e.target.value);
+    }
+    // 정렬
+    const onSortChange =(e)=>{
+        setSort(e.target.value)
+    }
+
+    // 검색
+    function search(){
+        // check
+        console.log("검색어 : " + query + "\nemotion : " + emotion + "\nstartDate : " + startDate + "\nendDate : " + endDate + "\nsort형식 : " + sort);
+        const data = new Object();
+        let url = "/v1/diaries/{userId}?"
+        // TODO 일기 검색 API에 query parameter로 넘기면 됨.
+        url = url+"query="+query+"&emotion="+emotion+"&startDate="+startDate+"&endDate="+endDate+"&sort="+startDate
+    }
+
+
     return (
       <div className="collapse rounded-3xl">
           <input type="checkbox" className="peer" /> 
@@ -20,28 +61,30 @@ const SearchBox = () => {
                             <div className="grid grid-cols-6 sm:gap-2">
                                 {/* 단어 검색 */}
                                 <div className="col-span-6 px-2 py-1 sm:col-span-3">
-                                    <label htmlFor="first-name" className="block text-base font-medium text-zinc-700">
+                                    <label htmlFor="search-word" className="block text-base font-medium text-zinc-700">
                                         검색어
                                     </label>
                                     <input
                                         type="text"
-                                        name="first-name"
-                                        id="first-name"
-                                        autoComplete="given-name"
-                                        placeholder='ex) 마카롱'
+                                        name="query"
+                                        id="query"
+                                        autoComplete="search-word"
+                                        placeholder='검색어를 입력하세요'
+                                        onChange={onQueryChange}
                                         className="w-full pl-4 mt-1 shadow-sm border-zinc-300 focus:outline-zinc-300 h-9 rounded-xl "
                                     />
                                 </div>
 
                                 {/* 감정 검색 */}
                                 <div className="col-span-6 px-2 py-1 sm:col-span-3">
-                                    <label htmlFor="country" className="block text-base font-medium text-zinc-700">
+                                    <label htmlFor="emotion" className="block text-base font-medium text-zinc-700">
                                         감정
                                     </label>
                                     <select
-                                        id="country"
-                                        name="country"
-                                        autoComplete="country-name"
+                                        id="emotion"
+                                        name="emotion"
+                                        autoComplete="emotion-value"
+                                        onClick={onEmotionChange}
                                         className="w-full px-3 py-2 mt-1 shadow-sm border-zinc-300 focus:outline-zinc-300 h-9 rounded-xl "
                                     >
                                         <option>전체</option>
@@ -62,18 +105,19 @@ const SearchBox = () => {
                                     <label htmlFor="first-name" className="block text-base font-medium text-zinc-700">
                                         기간
                                     </label>
-                                    <DateRangePicker/>
+                                    <DateRangePicker setStartDate={setStartDate} setEndDate={setEndDate} setDateFormat={setDateFormat} />
                                 </div>
 
                                 {/* 정렬 */}
                                 <div className="col-span-6 px-2 py-1 sm:col-span-3">
-                                    <label htmlFor="country" className="block text-base font-medium text-zinc-700 ">
+                                    <label htmlFor="sort" className="block text-base font-medium text-zinc-700 ">
                                         정렬
                                     </label>
                                     <select
-                                        id="country"
-                                        name="country"
-                                        autoComplete="country-name"
+                                        id="sort"
+                                        name="sort"
+                                        autoComplete="sort-value"
+                                        onClick={onSortChange}
                                         className="w-full px-3 py-2 mt-1 shadow-sm border-zinc-300 focus:outline-zinc-300 h-9 rounded-xl "
                                     >
                                         <option>오래된 순</option>
@@ -84,6 +128,7 @@ const SearchBox = () => {
                             <div className="flex justify-center">
                                 <button
                                     type="submit"
+                                    onClick={search}
                                     className="justify-center px-4 py-2 mx-1 mt-5 text-sm font-medium border border-transparent shadow-sm w-30 rounded-xl bg-primary text-primary-content hover:bg-primary-focus focus:outline-none focus:ring-2">
                                     검색하기🔍
                                 </button>
