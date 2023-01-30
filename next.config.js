ef4d55c8b8b91c6bd3be56c9a34cb7100c4d4662
ssoyeong <jsy171771@gmail.com>
2023-01-30 23:37:26 +0900

    SW-125 feat(config): Modify destination address

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
     return [
       {
         source: '/api/:path*',
-        destination: process.env.NEXT_PUBLIC_API_URL+'/:path*',
+        destination: process.env.NEXT_PUBLIC_API_URL+'/api/:path*',
       },
     ];
   },
