84bc4393af4a2f0273718fe25c4aa40c45c8a302
ssoyeong <jsy171771@gmail.com>
2023-01-31 14:45:36 +0900

    SW-130 design(dashboard): Modify profile card
    1. 대시보드의 내 프로필 카드에 최근 일기 정보 삭제
    2. 내 프로필 카드 ProfileCard로 컴포넌트명 변경

--- app/diary/dashboard/ProfileCard.js
+++ app/diary/dashboard/ProfileCard.js
@@ -1,17 +1,11 @@
 'use client';
 
-import userData from "../../../public/data/user.json";
 import Loading from "../list/grid/loading";
 import Error from "../list/grid/error";
 import { useUserInfoQuery } from "../../hooks/queries/useUserInfoQuery";
-import moment from 'moment';
 
-// app/common/header/Profile에서 거의 다 가져왔지만 일부 수정하기 위해 component 따로 생성.
 // export default function UserProfile(userInfo) {
-export default function UserProfile() {
-
-    // TODO: userInfo 응답으로 recentDiary 데이터 받으면 data-fetching 하기
-    const user = userData;
+export default function ProfileCard() {
 
     // 유저 정보 data fetching을 위한 useQuery
     const { data, isLoading, isFetching, isFetched, isError } = useUserInfoQuery();
@@ -45,40 +39,18 @@ export default function UserProfile() {
                     <div className='w-full'>
                         <div className="grid grid-cols-3">
                             {/* 총 작성한 일기 수 */}
-                            <div className='col-span-3 mb-1 sm:col-span-1'>
+                            <div className='col-span-3 mb-1'>
                                 <div className="mb-1 text-lg text-zinc-600">
-                                    작성한<br></br>일기장들
+                                    오늘까지 기록한 나의 일기
                                 </div>
                                 <div className='text-3xl font-bold'>
-                                    {data.diaryTotal}
+                                    {data.diaryTotal}개
                                 </div>
-                            </div>
-
-                            {/* 최근 일기 */}
-                            <div className='col-span-3 sm:col-span-2'>
-                              <div className="mb-1 text-lg text-zinc-600">
-                                최근 일기
-                              </div>
-                              <div className='mb-2 text-xl font-semibold'>
-                                {moment(user.recent_diaries[0].date).format("YYYY. MM. DD.")}
-                              </div>
-                              <div className='flex place-content-center'>
-                                <div className='w-1/3 pl-5 text-zinc-500'>
-                                  {user.recent_diaries[0].emotion}
-                                </div>
-                                <div className='relative flex'>
-                                  {user.recent_diaries[0].keywords.map((keyword) => (
-                                      <div key={keyword} className="px-2 py-1 mb-1 mr-2 text-xs font-medium w-fit text-zinc-500 bg-zinc-200 rounded-xl dark:bg-zinc-200 dark:text-zinc-800 ">
-                                          #{keyword}
-                                      </div>
-                                  ))}
-                                </div>
-                              </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </>
-        );
+    );
 }
--- app/diary/dashboard/page.jsx
+++ app/diary/dashboard/page.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import userData from "../../../public/data/user.json";
-import UserProfile from "./UserProfile";
 import CardBarChart from "./EmotionCompare";
 import DiaryCards from "./DiaryCards";
 import DoughnutChart from "./DoughnutChart";
 import { getUserInfo } from "../../api/getUserInfo";
 import { getCookie} from "cookies-next";
 import { cookies } from 'next/headers';
+import ProfileCard from "./ProfileCard";
 
 // async function getUserInfoData() {
 //     const nextCookies = cookies();
@@ -37,7 +37,7 @@ export default async function diaryPage() {
                     {/* 유저 프로필*/}
                     <div className="col-span-6 px-4 my-4 lg:col-span-2">
                         {/* <UserProfile userInfo={userInfo}/> */}
-                        <UserProfile/>
+                        <ProfileCard/>
                     </div>
                     {/* 이번 달 / 저번 달 감정빈도 비교 바 차트*/}
                     <div className="col-span-6 px-4 my-4 mb-10 lg:col-span-4 ">
