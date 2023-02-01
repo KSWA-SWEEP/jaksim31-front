373be254380ae6dd32b03ac9607b7d3ee9fceb75
ssoyeong <jsy171771@gmail.com>
2023-02-01 23:28:57 +0900

    SW-131 design(tutorial): Modify align grid items
    1. grid 정렬 수정
    2. 화면 캡쳐 이미지 추가
    3. tutorial 컴포넌트 함수명 변경

--- app/common/Tutorial.js
+++ app/common/Tutorial.js
@@ -1,26 +1,35 @@
 import LazyShow from "./LazyShow";
 import loginImage from "../../public/images/tutorial/login.png"
+import signUpImage from "../../public/images/tutorial/signUp.png"
+import drawerImage from "../../public/images/tutorial/drawer.png"
 import Image from "next/image";
 
 const examplePages = [
   {
-    title: "회원가입",
+    title: "로그인",
     src: loginImage,
+    alt: "login",
+    comment: "회원가입은 인증 메일을 통해 이루어집니다. 회원가입은 인증 메일을 통해 이루어집니다. 회원가입은 인증 메일을 통해 이루어집니다.회원가입은 인증 메일을 통해 이루어집니다. 회원가입은 인증 메일을 통해 이루어집니다. 회원가입은 인증 메일을 통해 이루어집니다회원가입은 인증 메일을 통해 이루어집니다. 회원가입은 인증 메일을 통해 이루어집니다. 회원가입은 인증 메일을 통해 이루어집니다 회원가입은 인증 메일을 통해 이루어집니다.",
+    index: 1,
+  },
+  {
+    title: "회원가입",
+    src: signUpImage,
     alt: "signUp",
     comment: "회원가입은 인증 메일을 통해 이루어집니다. 회원가입은 인증 메일을 통해 이루어집니다. 회원가입은 인증 메일을 통해 이루어집니다. 회원가입은 인증 메일을 통해 이루어집니다.",
     index: 1,
   },
   {
-    title: "로그인",
-    src: loginImage,
-    alt: "login",
+    title: "바로가기",
+    src: drawerImage,
+    alt: "drawer",
     comment: "회원가입은 인증 메일을 통해 이루어집니다. 회원가입은 인증 메일을 통해 이루어집니다. 회원가입은 인증 메일을 통해 이루어집니다. 회원가입은 인증 메일을 통해 이루어집니다.",
     index: 1,
   },
   {
-    title: "바로가기",
+    title: "일기 작성하기",
     src: loginImage,
-    alt: "drawer",
+    alt: "create",
     comment: "회원가입은 인증 메일을 통해 이루어집니다. 회원가입은 인증 메일을 통해 이루어집니다. 회원가입은 인증 메일을 통해 이루어집니다. 회원가입은 인증 메일을 통해 이루어집니다.",
     index: 1,
   },
@@ -55,17 +64,17 @@ const Tutorial = () => {
         <div>
           {examplePages.map((examplePage) => (
             <LazyShow delay={((examplePage.index)*0.5)-examplePage.index*0.2}>
-              <div className="py-4 flex items-stretch">
-                <div className="pr-4">
+              <div className="py-4 grid grid-cols-7">
+                <div className="pr-4 col-span-5">
                   <Image
                     src={examplePage.src}
                     alt={examplePage.alt}
                     placeholder={examplePage.alt}
                   />
                 </div>
-                <div className="grid grid-cols-1 grid-rows-4">
-                  <p className="row-start-2 text-2xl font-[GmarketSansMedium] font-semibold leading-8 text-zinc-900">{examplePage.title}</p>
-                  <p className="row-start-3 font-[GmarketSansLight] text-base leading-7 text-zinc-600">{examplePage.comment}</p>
+                <div className="col-span-2 grid grid-cols-1 grid-rows-5">
+                  <p className="row-span-1 text-3xl font-[GmarketSansMedium] font-semibold leading-8 text-zinc-900 pt-8">{examplePage.title}</p>
+                  <p className="row-span-4 font-[GmarketSansLight] text-base leading-7 text-zinc-600">{examplePage.comment}</p>
                 </div>
               </div>
             </LazyShow>
