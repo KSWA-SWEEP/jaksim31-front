2b4dadfacad5bf8b45c1cd827aec62fdaa642611
Song Yujin <j_jongleur@naver.com>
2023-02-08 03:11:43 +0900

    SW-151 feat(seo): Modify next-sitemap.config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
     siteUrl: 'https://jaksim31.xyz',
-    exclude: ['/404', '/diary/'],
+    exclude: ['/404'],
     generateRobotsTxt: true,
     robotsTxtOptions: {
       policies: [
         {
             userAgent: '*',
-            disallow: ['/404', '/diary/'],
+            disallow: ['/404'],
         },
         { 
             userAgent: '*', 
