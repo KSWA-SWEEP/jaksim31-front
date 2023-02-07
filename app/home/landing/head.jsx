2fb368946cf5822510fb1cf6ca84e2ac4ae15b0d
Song Yujin <j_jongleur@naver.com>
2023-02-07 19:40:33 +0900

    SW-151 feat(seo): Add head.js to SEO
    * SEO 적용을 위한 head.js 파일 추가 (경로별)
    * robots.txt / sitemap.xml 파일 추가
    * 아이콘 변경 (favicon.ico => jaksim31.ico)

---
+++ app/home/landing/head.jsx
@@ -0,0 +1,11 @@
+import DefaultTags from '../../DefaultTags';
+
+export default function Head() {
+  return (
+    <>
+      <title>작심삼일 | Main</title>
+      <meta name="description" content="작지만 심플하고 쌈박한 일기" />
+      <DefaultTags/>
+    </>
+  )
+}
