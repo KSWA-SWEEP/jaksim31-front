599f9c3749f9046f9da16d5d086d126738b34010
Song Yujin <j_jongleur@naver.com>
2023-02-10 22:15:29 +0900

    SW-154 feat(ssr): Modify server-side data fetching (gridList)

--- app/diary/list/grid/page.jsx
+++ app/diary/list/grid/page.jsx
@@ -1,12 +1,17 @@
 import { cookies } from "next/headers";
-import { getDiaryList } from "../../../api/getDiaryList";
-import { getUserInfo } from "../../../api/getUserInfo";
 import DiaryGridList from "./gridList";
 
-async function getDiaryListData(userId) {
-
-    const res = await fetch(process.env.NEXT_PUBLIC_API_URL + "/api/v1/diaries/" + userId + "?page=0&size=6", { cache: 'no-store' });
-
+async function getDiaryListData(userId, atk, rtk) {
+    let res = await fetch(
+        process.env.NEXT_PUBLIC_BASE_URL+"/api/v1/diaries/"+userId+"?page=0&size=6", 
+        {
+            cache : 'no-store', 
+            headers: {
+                Cookie: `atk=${atk}; rtk=${rtk};`
+            }
+        }
+    );
+    
     if (res.status != 200) {
         throw new Error('Failed to fetch data');
     }
@@ -17,7 +22,9 @@ async function getDiaryListData(userId) {
 export default async function gridList() {
     const cookieStore = cookies();
     const userId = cookieStore.get('userId');
-    const diaryList = await getDiaryListData(userId);
+    const atk = cookieStore.get('atk');
+    const rtk = cookieStore.get('rtk');
+    const diaryList = await getDiaryListData(userId.value, atk.value, rtk.value);
 
     return (
         <>
